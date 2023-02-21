3109b3a1093a196fe435e26a9570e3575efeb793
elifoksas <62424342+elifoksas@users.noreply.github.com>
2023-02-21 14:09:12 +0300

    inital commit

--- app/src/main/java/com/elifoksas/cryptoapp/view/MainActivity.kt
+++ app/src/main/java/com/elifoksas/cryptoapp/view/MainActivity.kt
@@ -1,11 +1,14 @@
-package com.elifoksas.cryptoapp
+package com.elifoksas.cryptoapp.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.elifoksas.cryptoapp.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
     }
 }
